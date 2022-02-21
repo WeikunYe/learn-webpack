dcf8a39a495431b51acdb91160febdbc5570bfcd
WeikunYe <weikunye1225@gmail.com>
2022-02-21 11:37:46 +1100

    feat/p8

---
+++ 06-打包其他资源/README.md
@@ -0,0 +1,24 @@
+# 备注
+
+## file-loader
+
+webpack@5 内置 file-loader 只需定义 `type` 即可
+
+```javascript
+{
+    exclude: /\.(html|js|css)/,
+    type: "asset/resource",
+}
+```
+
+## 对应关系
+
+webpack@4 三个 loader 对应 webpack@5 三个 type
+
+- `file-loader` 对应 `{type: "asset/resource"}`: emits a separate file and exports the URL
+- `url-loader` 对应 `{type: "asset/inline"}`: exports a data URI of the asset
+- `raw-loader` 对应 `{type: "asset/source"}`: exports the source code of the asset
+
+详细见[文档](https://webpack.js.org/guides/asset-modules/)
+
+webpack@5 asset module 特性也可以看这篇[技术博客](https://blog.csdn.net/lin_fightin/article/details/115140736)的前半部分, 感觉讲的挺清楚的
---
+++ 06-打包其他资源/webpack.config.js
@@ -0,0 +1,45 @@
+const { resolve } = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports={
+    entry: "./src/index",
+    output: {
+        filename: "built.js",
+        path:resolve(__dirname, "build")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:[
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.html$/,
+                use:{
+                    loader: "html-loader"
+                }
+            },
+            // 打包其他资源, 排除 css js html
+            // webpack@5 内置 file-loader, 使用 asset/resource type 即可
+            // 配置资源输出文件名和目录用 generator filename
+            {
+                //test: /\.(eot|svg|ttf|woff)$/,
+                exclude: /\.(html|js|css)/,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/[hash:10][ext]"
+                }
+
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    mode: "development"
+}
